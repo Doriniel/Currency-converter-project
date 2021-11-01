456c81e5174b00dd1f6880718c0f21a202c13638
Doriniel <doriniel@gmail.com>
2021-11-01 17:24:06 +0300

    01.11.21 17:20

--- script.js
+++ script.js
@@ -1,75 +1,107 @@
 
 // https://api.exchangerate.host/latest?base=USD&symbols=RUB 
 
-// let currencyFrom = "";
-// let currencyTo = "";
-// let amount = 0;
+
 
 let selector = document.querySelectorAll('.calc-currency-select');
-let button = document.querySelectorAll('.calc-currency-item');
-// const calcCurrencyBox = document.querySelector('.calc-currency');
+// let button = document.querySelectorAll('.calc-currency-item');
 
-button.forEach(item => {
-    item.addEventListener('click', function (event) {
-            if (event.target.tagName === "BUTTON") {
-                // console.log(event.target.innerText);
-                item.classList.toggle('active');
-            }
-        })
-});
-
+// button.forEach(item => {
+//     item.addEventListener('click', markActiveButton);
+// })
 
 
 // Получить rates от доллара к рублю через API: (сначала сделать для пар, которые уже есть в html-ке? потом для тех, которые в Селекте?):
+// https://api.exchangerate.host/latest?base=USD&symbols=RUB&amount=${amount}; 
+// https://api.exchangerate.host/latest?base=${currencyFrom}&symbols=${currencyTo}&amount=${amount}; 
 
-// https://api.exchangerate.host/latest?base=USD&symbols=RUB 
+let boxFrom;
+let boxTo;
+let currencyFrom;
+let currencyTo;
+let amount;
 
-function ;
+boxFrom = document.querySelector('.from');
+currencyFromBtn = boxFrom.querySelectorAll('.calc-currency-item');
+boxTo = document.querySelector('.to');
+currencyToBtn = boxTo.querySelectorAll('.calc-currency-item');
+amountField = document.querySelector('.calc-input-field');
 
+currencyFromBtn.forEach(item => {
+    item.addEventListener('click', (event) => {
+        currencyFrom = `${event.target.innerText}`;
+        console.log(currencyFrom);
+    })
 
-
-let currencyArr = [];
-function getCurrency() {
-    fetch('https://api.exchangerate.host/latest')
-        .then(response => response.json())
-        .then(data => {
-            currencyArr = Object.keys(data.rates);
-            currencyArr.forEach(item => {
-                renderSelect(item);
-            })
-        })
-}
-
-
-
-
-// Добавляем обоим селекторам обработчики событий по типу change (т.е. на изменение элемента):
-selector.forEach((item) => {
-    item.addEventListener('change', selectorHandler);
-
+    item.addEventListener('click', (event) => {
+        if (event.target.tagName === "BUTTON") {
+            item.classList.toggle('active');
+        }
+    })
 })
 
-    
-function selectorHandler (event) {
+
+currencyToBtn.forEach(item => {
+    item.addEventListener('click', (event) => {
+        currencyTo = `${event.target.innerText}`;
+        console.log(currencyTo);
+    })
+
+    item.addEventListener('click', (event) => {
+        if (event.target.tagName === "BUTTON") {
+            item.classList.toggle('active');
+        }
+    })
+})
+
+amountField.addEventListener('change', (event) => {
+    amount = `${event.target.value}`;
+    console.log(amount);
+})
+
+
+function getConverted(currencyFrom, currencyTo, amount) {
+    return new Promise((resolve, reject) => {
+        fetch(`https://api.exchangerate.host/latest?base=${currencyFrom}&symbols=${currencyTo}&amount=${amount}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.rates);
+                resolve(data);
+            })
+            .catch(error => {
+                reject(error);
+            })
+    })
+}
+
+getConverted('USD', 'RUB', 2000);
+
+
+// console.log(data.rates);
+
+// const getConverted = async (currencyFrom, currencyTo, amount) => {
+//     const response = await fetch(`https://api.exchangerate.host/latest?base=${currencyFrom}&symbols=${currencyTo}&amount=${amount}`);
+//     const data = await response.json();
+//     return data
+// }
+
+// ____________________________________________________________________________________________//
+
+
+// Добавляем обоим селекторам обработчики событий по типу change (т.е. на изменение элемента?):
+selector.forEach((item) => {
+    item.addEventListener('change', selectorHandler);
+})
+
+function selectorHandler(event) {
     if (event.target.value) {
         let selected = document.querySelector('option');
         selected.classList.toggle('active');
         // this.classList.toggle('active');
     }
-        // selector.options[selector.selectedIndex].value);
-};    
+    // selector.options[selector.selectedIndex].value);
+};
 
-
-// wrapper.forEach((item) => {
-//     item.addEventListener('change', (event) => {
-//         if(event.target.tagName ==='SELECT') {
-//           if (event.target.value==="2") {
-//             console.log(this.innerText)};
-//         }
-//       })
-// })
-
-  
 getCurrency();
 let currencyArr = [];
 function getCurrency() {
@@ -87,7 +119,6 @@ function renderSelect(item) {
     let selectors = document.querySelectorAll('.calc-currency-select');
     selectors.forEach((elem) => {
         let option = new Option(`${item}`, `${item}`);
-        // console.log(option);
         elem.append(option);
         // let currOption = document.createElement('option');
         // currOption.innerText = `${item}`;
